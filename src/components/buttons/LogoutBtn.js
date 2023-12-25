e808161234f99f4d5769a871de180a8054de0b80
Sarthak Kamble <sarthakkamble412@gmail.com>
2023-12-25 23:24:25 +0530

    Account Page Sidebar implemented with logout functionality.

--- src/components/buttons/LogoutBtn.js
+++ src/components/buttons/LogoutBtn.js
@@ -5,14 +5,15 @@ import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { signOut } from "next-auth/react";
 
-export default function LogoutBtn() {
+export default function LogoutBtn({
+  className = "bg-black/90 px-3 py-2 rounded flex items-center gap-2 text-white hover:bg-purple-700 transition-all duration-300",
+  iconLeft = false,
+}) {
   return (
-    <button
-      onClick={() => signOut()}
-      className="bg-black/90 px-3 py-2 rounded flex items-center gap-2 text-white hover:bg-purple-700 transition-all duration-300"
-    >
+    <button onClick={() => signOut()} className={className}>
+      {iconLeft && <FontAwesomeIcon icon={faArrowRightFromBracket} />}
       <span>Logout</span>
-      <FontAwesomeIcon icon={faArrowRightFromBracket} />
+      {!iconLeft && <FontAwesomeIcon icon={faArrowRightFromBracket} />}
     </button>
   );
 }
