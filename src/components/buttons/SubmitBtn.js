3b438c42308044c347458c3742da535442cbfd15
Sarthak Kamble <sarthakkamble412@gmail.com>
2023-12-27 02:08:56 +0530

    Made changes in the SubmitBtn UI for better UX

--- src/components/buttons/SubmitBtn.js
+++ src/components/buttons/SubmitBtn.js
@@ -1,15 +1,15 @@
 import { useFormStatus } from "react-dom";
 
-export default function SubmitBtn({ children }) {
+export default function SubmitBtn({ children, className }) {
   const { pending } = useFormStatus();
 
   return (
     <button
       type="submit"
       disabled={pending}
-      className="bg-black w-full text-white disabled:bg-purple-300 disabled:text-gray-200 px-4 py-2.5 flex mx-auto items-center justify-center gap-2 rounded-md hover:bg-purple-800 transition-all duration-300"
+      className={`bg-black w-full text-white disabled:bg-purple-400 disabled:text-white px-4 py-2.5 flex mx-auto items-center justify-center gap-2 rounded-md hover:bg-purple-800 transition-all duration-300 ${className}`}
     >
-      {children}
+      {pending ? <span>Saving...</span> : children}
     </button>
   );
 }
