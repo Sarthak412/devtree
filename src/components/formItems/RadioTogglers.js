c645377a896a0addb0078b21abf35c1433830a9b
Sarthak Kamble <sarthakkamble412@gmail.com>
2023-12-27 23:53:15 +0530

    User interface changed for the RadioTogglers component

--- src/components/formItems/RadioTogglers.js
+++ src/components/formItems/RadioTogglers.js
@@ -1,14 +1,20 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function RadioTogglers({ options }) {
+export default function RadioTogglers({ options, defaultValue, onChange }) {
   return (
     <div className="radio-togglers shadow">
       {options.map((option) => (
         <label key={option.label}>
-          <input type="radio" name="bgType" value={option.value} />
-          <div className=" -mt-16 mr-3 rounded-full">
+          <input
+            type="radio"
+            name="bgType"
+            onClick={(e) => onChange(e.target.value)}
+            defaultChecked={defaultValue === option.value}
+            value={option.value}
+          />
+          <div>
             <FontAwesomeIcon icon={option.icon} className="h-5" />
-            {/* <span>{option.label}</span> */}
+            <span>{option.label}</span>
           </div>
         </label>
       ))}
