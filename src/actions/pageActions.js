9ca305ca53a22a25b0ab77413cf46bb10b7393db
Sarthak Kamble <sarthakkamble412@gmail.com>
2023-12-28 15:53:06 +0530

    Created the User model for uploading and changing profile image.

--- src/actions/pageActions.js
+++ src/actions/pageActions.js
@@ -2,6 +2,7 @@
 
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { Page } from "@/models/Page";
+import { User } from "@/models/User";
 import mongoose from "mongoose";
 import { getServerSession } from "next-auth";
 
@@ -30,6 +31,14 @@ export async function saveProfileInformation(formData) {
     }
 
     await Page.updateOne({ owner: session?.user?.email }, dataToUpdate);
+
+    if (formData.has("avatar")) {
+      const avatarLink = formData.get("avatar");
+      await User.updateOne(
+        { email: session.user?.email },
+        { image: avatarLink }
+      );
+    }
 
     return true;
   }
