ec113737dcef1dc9e596303bd966fe67d769b46a
Sarthak Kamble <sarthakkamble412@gmail.com>
2023-12-29 21:31:28 +0530

    Server action added for saving social links to pages model.

--- src/actions/pageActions.js
+++ src/actions/pageActions.js
@@ -45,3 +45,25 @@ export async function saveProfileInformation(formData) {
 
   return false;
 }
+
+export async function savePageButtons(formData) {
+  // Connecting to DB
+  mongoose.connect(process.env.MONGO_URI);
+
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    // Object to store Social Links in key value pairs
+    const linkValues = {};
+
+    formData.forEach((value, key) => {
+      linkValues[key] = value;
+    });
+
+    const dataToUpdate = { socialLinks: linkValues };
+    await Page.updateOne({ owner: session?.user?.email }, dataToUpdate);
+    return true;
+  }
+
+  return false;
+}
