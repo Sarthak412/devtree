9d4060bba1e194c9147c2daab2655ca99d18b9e7
Sarthak Kamble <sarthakkamble412@gmail.com>
2024-01-03 22:21:42 +0530

    Developed themes for dark mode and white mode integrated.

--- src/app/(page)/[uri]/page.js
+++ src/app/(page)/[uri]/page.js
@@ -2,6 +2,7 @@ import { ModeToggle } from "@/components/buttons/DarkModeToggle";
 import { socialButtons } from "@/constants";
 import { Page } from "@/models/Page";
 import { User } from "@/models/User";
+import LeftSlide from "@/utils/LeftSlide";
 import {
   faDiscord,
   faGithub,
@@ -10,7 +11,12 @@ import {
   faXTwitter,
   faYoutube,
 } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope, faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowUpRightFromSquare,
+  faEnvelope,
+  faImage,
+  faLocationDot,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import mongoose from "mongoose";
 import Image from "next/image";
@@ -38,7 +44,7 @@ export default async function DevtreePage({ params }) {
   console.log(page);
 
   return (
-    <div className="bg-white dark:bg-black/95">
+    <div className="bg-white/80 min-h-screen dark:bg-black/95">
       <div
         className="h-36 bg-gray-300 bg-cover bg-center"
         style={
@@ -71,7 +77,7 @@ export default async function DevtreePage({ params }) {
       <div className="max-w-sm mx-auto text-center p-2 my-2">
         <h2 className="">{page.bio}</h2>
       </div>
-      <div className="flex gap-2 justify-center pb-4">
+      <div className="flex gap-2 justify-center mt-4 pb-4">
         {Object.keys(page.socialLinks).map((socialLink) => (
           <Link
             key={socialLink}
@@ -80,6 +86,66 @@ export default async function DevtreePage({ params }) {
           >
             <FontAwesomeIcon className="w-5 h-5" icon={icons[socialLink]} />
           </Link>
+        ))}
+      </div>
+      {/* Project Links */}
+
+      <div className="max-w-6xl mx-auto p-4 px-8 ">
+        {page.projectLinks.map((link) => (
+          <div
+            key={link.key}
+            className="text-black p-2 mb-4 block border-2 border-gray-900 dark:border-gray-300 py-4 rounded dark:text-gray-200 shadow-neurobrutalism_black flex items-center gap-5 backdrop-filter backdrop-blur-md bg-opacity-20 transition-all duration-300 hover:bg-gradient-to-br hover:from-pink-200/20 hover:to-purple-500/60 hover:dark:bg-gradient-to-br hover:dark:from-purple-500/20 hover:dark:to-purple-800/5 hover:dark:border-purple-700"
+          >
+            {link.icon && (
+              <div className="w-1/4 md:w-1/5 md:border-2 md:border-gray-800 relative -left-6 border hidden dark:md:border-gray-300 py-2 px-2 md:block">
+                <Image
+                  src={link.icon}
+                  alt={"project logo"}
+                  width={250}
+                  height={250}
+                  objectFit="cover"
+                  className="w-full h-full object-cover"
+                />
+              </div>
+            )}
+
+            {/* If icon is not present it loads a default icon */}
+            {!link.icon && (
+              <div className="w-1/2 md:w-1/5 relative flex items-center justify-center bg-purple-400/40 border-2 py-5 -left-6">
+                <FontAwesomeIcon icon={faImage} className="h-20" />
+              </div>
+            )}
+
+            <div className="flex flex-col w-full md:w-3/4">
+              <div>
+                <h3 className="text-2xl text-black dark:text-white font-semibold py-1">
+                  {link.projectTitle}
+                </h3>
+                <p className="mb-1 text-gray-800 dark:text-gray-200">
+                  {link.description}
+                </p>
+                <p className="text-gray-700 dark:text-gray-300">
+                  <span className="font-semibold text-black dark:text-white">
+                    Technologies:
+                  </span>{" "}
+                  {link.technologies}
+                </p>
+              </div>
+              <div className="mt-4 flex gap-4">
+                <button className="bg-black border border-purple-500 text-white px-4 py-2 rounded-md flex items-center gap-2 hover:scale-95 shadow-neurobrutalism hover:shadow-none">
+                  <FontAwesomeIcon
+                    icon={faArrowUpRightFromSquare}
+                    className="h-4"
+                  />
+                  <span>Live Link</span>
+                </button>
+                <button className="bg-black border border-red-400 text-white px-4 py-2 rounded-md flex gap-2 hover:scale-95 shadow-neurobrutalism_two hover:shadow-none">
+                  <FontAwesomeIcon icon={faGithub} className="h-5" />
+                  <span>GitHub</span>
+                </button>
+              </div>
+            </div>
+          </div>
         ))}
       </div>
     </div>
