0d95bdd584e8b2c80ef3dffb9a36c8642eecbcf8
Sarthak Kamble <sarthakkamble412@gmail.com>
2024-01-05 00:08:26 +0530

    Added the Chart Component with dummy data on Analytics Page.

--- src/app/(app)/analytics/page.js
+++ src/app/(app)/analytics/page.js
@@ -1,3 +1,68 @@
-export default function AnalyticsPage() {
-  return <div>Analytics page</div>;
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import Chart from "@/components/Chart";
+import SectionBox from "@/components/layout/SectionBox";
+import { Event } from "@/models/Event";
+import { Page } from "@/models/Page";
+import mongoose from "mongoose";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+
+export default async function AnalyticsPage() {
+  // Connect to DB
+  mongoose.connect(process.env.MONGO_URI);
+
+  // Getting session
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/");
+  }
+
+  const page = await Page.findOne({ owner: session?.user?.email });
+
+  // const viewsCount = await Event.countDocuments({
+  //   type: "view",
+  //   uri: page.uri,
+  // });
+
+  // const githubClickCount = await Event.countDocuments({
+  //   type: "click",
+  //   uri: page.projectLinks.map((l) => l.githubLink),
+  // });
+
+  // const liveLinkCount = await Event.countDocuments({
+  //   type: "click",
+  //   uri: page.projectLinks.map((l) => l.liveLink),
+  // });
+
+  // Aggredating 1st stage
+  const groupedViews = await Event.aggregate([
+    {
+      $match: {
+        type: "view",
+        uri: "Sarthakkk",
+      },
+    },
+    {
+      $group: {
+        _id: {
+          $dateToString: {
+            date: "$createdAt",
+            format: "%Y-%m-%d",
+          },
+        },
+        count: {
+          $count: {},
+        },
+      },
+    },
+  ]);
+
+  return (
+    <div>
+      <SectionBox>
+        <Chart />
+      </SectionBox>
+    </div>
+  );
 }
