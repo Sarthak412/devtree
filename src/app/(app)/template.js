f6a6240266dee30f327b18b4802df54b38b4ddbf
Sarthak Kamble <sarthakkamble412@gmail.com>
2024-01-10 13:01:37 +0530

    Responsive Navbar for mobile screens integrated.

--- src/app/(app)/template.js
+++ src/app/(app)/template.js
@@ -5,6 +5,8 @@ import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
 import Image from "next/image";
 
+import "../globals.css";
+
 import LogoutBtn from "@/components/buttons/LogoutBtn";
 
 import AppSidebar from "@/components/layout/AppSidebar";
@@ -35,7 +37,7 @@ export default async function AppTemplate({ children }) {
 
   return (
     <html lang="en">
-      <body className={`${inter.className} flex`}>
+      <body className={`${inter.className} md:flex`}>
         <Toaster
           position="top-center"
           reverseOrder={false}
@@ -55,8 +57,20 @@ export default async function AppTemplate({ children }) {
             },
           }}
         />
-        <main className="md:flex min-h-screen">
-          <aside className="bg-white border-r shadow -translate-x-56 md:translate-x-0 shadow-gray-300 w-56 p-8 flex flex-col pt-8 fixed md:static left-0 top-0 bottom-0 z-20">
+        <main className="md:flex md:min-h-screen">
+          <label
+            for="navCb"
+            className="md:hidden ml-8 mt-6 bg-white px-5 py-3 shadow rounded-md inline-flex items-center gap-3 cursor-pointer"
+          >
+            <FontAwesomeIcon icon={faBars} />
+            <span>Dashboard</span>
+          </label>
+          <input type="checkbox" id="navCb" className="hidden" />
+          <label
+            for="navCb"
+            className="hidden backdrop fixed inset-0 bg-black/80 z-10"
+          ></label>
+          <aside className="bg-white border-r shadow md:translate-x-0 shadow-gray-300 w-56 p-8 flex flex-col pt-8 fixed md:static -left-56 top-0 bottom-0 z-20 transition-all duration-200">
             <div className="sticky top-0 pt-2">
               <div className="flex items-center justify-center rounded-full overflow-hidden border-[4px] border-gray-200 shadow-md shadow-gray-400 aspect-square w-28 mx-auto">
                 <Image
