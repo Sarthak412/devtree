974d0b82a612b4d94bb66e86f929f9e98f64ec2f
Sarthak Kamble <sarthakkamble412@gmail.com>
2024-01-11 14:50:12 +0530

    Fixed the responsiveness of HeroForm component

--- src/components/forms/HeroForm.js
+++ src/components/forms/HeroForm.js
@@ -36,17 +36,18 @@ export default function HeroForm({ user }) {
   return (
     <form
       onSubmit={handleSubmit}
-      className="inline-flex border rounded bg-white items-center shadow-md"
+      className="inline-flex items-center rounded-r-md bg-white shadow-md"
     >
-      <span className="bg-white text-black py-2 pl-2">devtree.to/</span>
+      <span className="bg-white text-black py-3 pl-2">devtree.to/</span>
       <input
         className="py-2 border-none outline-none bg-white text-gray-500"
         type="text"
+        placeholder="username"
         onChange={(e) => setUsername(e.target.value)}
       />
       <button
         type="submit"
-        className="bg-purple-700 text-white rounded-r-md px-4 py-2 hover:bg-purple-600"
+        className="bg-purple-700 text-white rounded-r-md px-4 py-3 hover:bg-purple-600 relative -ml-16 md:ml-0 whitespace-nowrap"
       >
         Join for free
       </button>
