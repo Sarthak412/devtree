c033ef383248c4360adb17d4fb6c5bd7e6b33686
Sarthak Kamble <sarthakkamble412@gmail.com>
2024-01-12 23:09:30 +0530

    minor changes in the UI

--- src/app/(app)/template.js
+++ src/app/(app)/template.js
@@ -67,10 +67,10 @@ export default async function AppTemplate({ children }) {
           </label>
           <input type="checkbox" id="navCb" className="hidden" />
           <label
-            for="navCb"
+            htmlFor="navCb"
             className="hidden backdrop fixed inset-0 bg-black/80 z-10"
           ></label>
-          <aside className="bg-white border-r shadow md:translate-x-0 shadow-gray-300 w-50 p-8 flex flex-col pt-8 fixed md:static -left-56 top-0 bottom-0 z-20 transition-all duration-200">
+          <aside className="bg-white border-r shadow md:translate-x-0 shadow-gray-300 w-50 p-10 flex flex-col pt-8 fixed md:static -left-56 top-0 bottom-0 z-20 transition-all duration-200">
             <div className="sticky top-0 pt-2">
               <div className="flex items-center justify-center rounded-full overflow-hidden border-[4px] border-gray-200 shadow-md shadow-gray-400 aspect-square w-28 mx-auto">
                 <Image
@@ -93,7 +93,7 @@ export default async function AppTemplate({ children }) {
                     size="lg"
                   />
                   <span className="text-gray-300 text-xl">/</span>
-                  <span className="text-gray-500">{page.uri}</span>
+                  <span className="text-gray-500 text-xl">{page.uri}</span>
                 </Link>
               )}
               <div className="text-center flex-grow flex flex-col mx-auto">
