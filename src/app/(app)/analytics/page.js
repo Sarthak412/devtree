0411473070b7bb2e247cda18a746d887c2428725
Sarthak Kamble <sarthakkamble412@gmail.com>
2024-01-15 14:25:56 +0530

    Issue fix: Conditional rendering added to load 'totalClicksForToday' if clicks are present.

--- src/app/(app)/analytics/page.js
+++ src/app/(app)/analytics/page.js
@@ -93,6 +93,8 @@ export default async function AnalyticsPage() {
     },
   ]);
 
+  const hasTotalClicks = totalClicksToday.length > 0;
+
   return (
     <div className="flex flex-col">
       <SectionBox>
@@ -133,18 +135,32 @@ export default async function AnalyticsPage() {
           </ul>
         </div>
 
-        <div className="w-fit bg-white m-8 p-5 shadow mt-4">
-          {totalClicksToday.map((click) => (
-            <div key={click.totalClicksForToday}>
+        {/* Checking if total clicks are present if null it renders the total clicks to 0 otherwise it renders the total clicks count */}
+        {hasTotalClicks ? (
+          <div className="w-fit bg-white m-8 p-5 shadow mt-4">
+            {totalClicksToday.map((click) => (
+              <div key={click.totalClicksForToday}>
+                <h1 className="text-2xl text-gray-500 font-semibold mb-4 mt-1 px-2 text-center">
+                  Total Clicks Today
+                </h1>
+                <h1 className="text-7xl text-center my-12  font-extrabold text-purple-800/70">
+                  {click.totalClicksForToday}
+                </h1>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="w-fit bg-white m-8 p-5 shadow mt-4">
+            <div>
               <h1 className="text-2xl text-gray-500 font-semibold mb-4 mt-1 px-2 text-center">
                 Total Clicks Today
               </h1>
               <h1 className="text-7xl text-center my-12  font-extrabold text-purple-800/70">
-                {click.totalClicksForToday}
+                0
               </h1>
             </div>
-          ))}
-        </div>
+          </div>
+        )}
       </div>
     </div>
   );
